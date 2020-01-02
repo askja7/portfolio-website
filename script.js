e373ffeec7739fbc08a4968e4b0f54778f9d49db
askja7 <staneva.lora@gmail.com>
2020-01-02 23:03:28 +0100

    step-one

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+/**HOME PAGE EFFECT**/
+
+const lines = document.querySelectorAll(".line");
+
+window.addEventListener("load", start);
+
+function start() {
+  lines.forEach(drawThis);
+}
+
+
+function drawThis(line, i){
+    setTimeout(function(){
+      line.classList.add("draw");
+    }, i * 2000);
+
+
+}
+
+/**PORTFOLIO PAGE EFFECT**/
+
 function rollover(my_image)
     {
 
-        my_image.src = 'images/basic-web-hover-01.svg';
+        my_image.src = 'images/basic-web-hover.svg';
 
     }
 
  function mouseaway(my_image)
     {
 
-        my_image.src = "images/test1.png";
+        my_image.src = "images/basic-web.svg";
 
     }
+
