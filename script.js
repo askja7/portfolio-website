e2542339e8428d83b15909987b76bc7518e20459
askja7 <staneva.lora@gmail.com>
2020-01-02 18:40:37 +0100

    step-one

---
+++ script.js
@@ -0,0 +1,13 @@
+function rollover(my_image)
+    {
+
+        my_image.src = 'images/basic-web-hover-01.svg';
+
+    }
+
+ function mouseaway(my_image)
+    {
+
+        my_image.src = "images/test1.png";
+
+    }
